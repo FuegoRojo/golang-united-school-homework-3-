242531da41e645303baf2328fead3aee1b4bccb6
FuegoRojo <99490471+FuegoRojo@users.noreply.github.com>
2022-05-19 10:59:10 +0300

    Update task01-arrays.go

--- task01-arrays.go
+++ task01-arrays.go
@@ -1,6 +1,15 @@
 package homework
 
 func average(input [15]float32) (result float32) {
-	//Place your code here
-	return
+        var sum float32 = 0
+        var item_qty float32 = 0
+        for _, myitem := range input{
+                sum = sum+myitem
+                item_qty = item_qty + 1
+        }
+        //print(int(item_qty))
+        //print(" ")
+        //Place your code here
+        return sum/float32(item_qty)
+
 }
